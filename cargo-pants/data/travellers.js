c7a90e264868171bc0705c9c890c8fba7ec4de96
Łukasz Gola <105663541+lukasgola@users.noreply.github.com>
2023-05-20 01:08:52 +0200

    lofiusers

---
+++ cargo-pants/data/travellers.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 1,
+        firstName: 'John',
+        secondName: 'Smith',
+        photo: require('../assets/1.webp')
+    },
+    {
+        id: 2,
+        firstName: 'Jack',
+        secondName: 'Wolski',
+        photo: require('../assets/2.jpeg')
+    },
+    {
+        id: 3,
+        firstName: 'William',
+        secondName: 'Peterson',
+        photo: require('../assets/3.jpeg')
+    }
+]
--- cargo-pants/src/Travellers.js
+++ cargo-pants/src/Travellers.js
@@ -1,14 +1,120 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Pressable, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
 import { useEffect, useState } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+
+
+import { Ionicons } from '@expo/vector-icons';
+
+import { colors } from '../theme/Theme';
+
+import travellers from '../data/travellers'
+
 
 export default function Travellers() {
 
 
+    const navigation = useNavigation();
+
+  const Item = ({item, navigation}) => {
+    return(
+      <TouchableOpacity style={{
+        width: '90%',
+        height: 100,
+        marginLeft: '5%',
+        marginTop: 20,
+        borderRadius: 10,
+        backgroundColor: colors.background,
+        flexDirection: 'row'
+      }}
+      //onPress={() => navigation.navigate('TripDetails', {item})}
+      >
+        
+        <Image 
+          source={item.photo} 
+          style={{
+            width: 80,
+            height: 80,
+            borderRadius: 40,
+            marginTop: 10,
+            marginLeft: 10,
+          }}
+        />
+        <View style={{marginLeft: 10}}>
+          <View style={{
+            width: 290,
+            height: 100,
+            justifyContent: 'space-between',
+            padding: 10,
+          }}>
+            <View style={{flexDirection: 'row', height: 20}}>
+              <Text style={{
+                color: item.current ? colors.text : colors.grey_d,
+                fontSize: 18,
+                fontWeight: 'bold'
+              }}>{item.firstName}</Text>
+              <Text style={{
+                color: item.current ? colors.text : colors.grey_d,
+                fontSize: 18,
+                fontWeight: '300'
+              }}> {item.secondName}</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <View style={{
+                width: '30%',
+                height: 40,
+                backgroundColor: '#f3f3f2',
+                marginRight: '3%',
+                borderRadius: 5,
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}>
+                <Ionicons name='call-outline' size={25} color={colors.text} />
+              </View>
+              <View style={{
+                width: '30%',
+                height: 40,
+                backgroundColor: '#f3f3f2',
+                marginRight: '3%',
+                borderRadius: 5,
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}>
+                <Ionicons name='chatbubbles-outline' size={25} color={colors.text} />
+              </View>
+              <View style={{
+                width: '30%',
+                height: 40,
+                backgroundColor: '#f3f3f2',
+                borderRadius: 5,
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}>
+                <Ionicons name='location-outline' size={25} color={colors.text} />
+              </View>
+            </View>
+          </View>
+          
+        </View>
+        
+
+      </TouchableOpacity>
+    )
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Travellers</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        style={{
+          width: '100%'
+        }}
+        data={travellers}
+        renderItem={({item}) => <Item item={item} navigation={navigation} />}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
